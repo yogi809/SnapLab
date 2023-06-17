fe0dd18cd8deaa43cf6f14c71f0f3fb48df9e168
Yogi <hntyg1089@gmail.com>
2023-06-17 11:31:26 +0900

    [Add] 日本語化・時刻の設定

--- config/application.rb
+++ config/application.rb
@@ -29,10 +29,13 @@ module SnapLab
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
