2be1eab26012047a823b348e1a17e4f78284f60f
Yogi <hntyg1089@gmail.com>
2023-06-20 10:21:53 +0900

    [Add] users_controllerとviewを追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to login_path, notice: 'User registered successfully!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+end
