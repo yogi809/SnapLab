c4acc9ebc7f20a14571349f55580493702492bc1
Yogi <hntyg1089@gmail.com>
2023-06-20 15:18:51 +0900

    [Add] controllerとviewを追加

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,47 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to @post, notice: 'Post created successfully.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post updated successfully.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: 'Post deleted successfully.'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :image)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  resources :posts
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'login' => "user_sessions#create"
