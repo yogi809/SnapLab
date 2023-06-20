5bdaf99d7eb5f45c03dc3647b6d81465776a1303
Yogi <hntyg1089@gmail.com>
2023-06-20 13:08:11 +0900

    [Add] Postモデルを追加

---
+++ db/migrate/20230620035800_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :title
+      t.string :description
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
