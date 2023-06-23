bdfffaa345c75229475b24f5dd35e5aa981d90e6
Yogi <hntyg1089@gmail.com>
2023-06-23 19:26:11 +0900

    [Fix] n+1問題が起こらないように修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :check_authorization, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.includes(:user).order(created_at: :desc)
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  resources :users
-  resources :posts
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users
+  resources :posts
 end
