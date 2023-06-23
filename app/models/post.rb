c2252b818abe0e6044f775f6e741bc682c55defb
Yogi <hntyg1089@gmail.com>
2023-06-23 17:45:52 +0900

    [Add] Postモデルにバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -23,5 +23,6 @@ class Post < ApplicationRecord
   has_one_attached :image
 
   validates :title, presence: true, length: { maximum: 255 }
+  validates :description, length: { maximum: 255 }
   validates :image, presence: true
 end
